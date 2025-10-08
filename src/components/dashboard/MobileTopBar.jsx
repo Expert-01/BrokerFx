2990a1ca1716190382a34ea9b41438a2486b7b1e
Expert-01 <soalagideon@gmail.com>
2025-10-08 08:21:51 +0100

    Update MobileTopBar.jsx

--- src/components/dashboard/MobileTopBar.jsx
+++ src/components/dashboard/MobileTopBar.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import MobileSidebar from "./MobileSidebar";
 import HamburgerIcon from "@/components/UI/HamburgerIcon";
-
+import Sidebar from "./Sidebar";
 function MobileTopBar() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   return (
     <>
       <div className="flex items-center justify-between mb-2">
         <span className="text-xl font-bold tracking-wide text-yellow-400">NexaExchange</span>
-        {/*<HamburgerIcon open={sidebarOpen} onClick={() => setSidebarOpen((v) => !v)} />*/}
-        <MobileSidebar open={sidebarOpen} onClose={() => setSidebarOpen(false)} />
+        {/*<HamburgerIcon open={sidebarOpen} onClick={() => setSidebarOpen((v) => !v)} />
+        <MobileSidebar open={sidebarOpen} onClose={() => setSidebarOpen(false)} />*/}
+        <Sidebar/>
       </div>
       
     </>
